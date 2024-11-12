54f0f67c31348d6c7f1b9aadbffa6647b371d930
Yun Seohyun <seohyunyun325@gmail.com>
2024-11-12 15:07:00 +0900

    Bean

---
+++ src/main/java/com/example/todoapi/hw/MyService.java
@@ -0,0 +1,16 @@
+package com.example.todoapi.hw;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MyService {
+
+    private final MyRepository myRepository;
+
+    public void serviceMethod() {
+        System.out.println("service");
+        myRepository.repositoryMethod();
+    }
+}
